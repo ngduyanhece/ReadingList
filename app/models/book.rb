ef6b768ec996c73f0f4dd8b2ce4abe2bd156b24a
ngduyanhece <ngduyanhece@gmail.com>
2015-11-23 13:32:14 +0700

    add search to this page

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ActiveRecord::Base
   scope :finished, ->{where.not(finished_on: nil)}
   scope :recent, ->{where('finished_on > ?', 2.days.ago)}
-
+  scope :search, ->(keyword){where(title: keyword) if keyword.present?}
 	def finished?
     finished_on.present?
   end
