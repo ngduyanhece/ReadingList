3958f6f82411d88d9cd1c2eea63ace39a6fc3aaa
ngduyanhece <ngduyanhece@gmail.com>
2015-11-23 13:56:25 +0700

    Improve search with keywords

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123043345) do
+ActiveRecord::Schema.define(version: 20151123064620) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20151123043345) do
     t.datetime "updated_at",  null: false
     t.integer  "rating"
     t.date     "finished_on"
+    t.text     "keywords"
   end
 
 end
